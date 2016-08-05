d4d44f1d8c3c9621222beaf12ac68cc1e422660d
Gustavo Fonseca <gustavo@gfonseca.net>
2016-08-05 18:18:51 -0300

    Refactoring to make stylechecker more reusable

--- packtools/stylechecker.py
+++ packtools/stylechecker.py
@@ -8,7 +8,6 @@ import json
 import logging
 
 from lxml import etree
-# import pygments if here
 
 import packtools
 from packtools import exceptions
@@ -32,43 +31,69 @@ def get_xmlvalidator(xmlpath, no_network, extra_sch):
     return packtools.XMLValidator.parse(parsed_xml, extra_schematron=extra_sch)
 
 
-def summarize(validator, assets_basedir=None):
+class StyleChecker(object):
+    def __init__(self, validator, assets_basedir=None):
+        self.validator = validator
+        self.assets_basedir = assets_basedir
 
-    def _make_err_message(err):
-        """ An error message is comprised of the message itself and the
-        element sourceline.
-        """
-        err_msg = {'message': err.message}
+    @classmethod
+    def fromfile(cls, file, assets_basedir=None, nonetwork=True, extrasch=None):
+        validator = get_xmlvalidator(file, nonetwork, extrasch)
 
-        try:
-            err_element = err.get_apparent_element(validator.lxml)
-        except ValueError:
-            LOGGER.info('Could not locate the element name in: %s' % err.message)
-            err_element = None
-
-        if err_element is not None:
-            err_msg['apparent_line'] = err_element.sourceline
+        # remote XML will not lookup for assets
+        if file.startswith(('http:', 'https:')):
+            assetsdir = None
         else:
-            err_msg['apparent_line'] = None
+            assetsdir = assets_basedir or os.path.dirname(file)
 
-        return err_msg
+        return cls(validator, assetsdir)
 
+    def annotate(self, buff, encoding=None):
+        _encoding = encoding or self.validator.encoding
+        err_xml = self.validator.annotate_errors()
 
-    dtd_is_valid, dtd_errors = validator.validate()
-    sps_is_valid, sps_errors = validator.validate_style()
+        buff.write(etree.tostring(err_xml, pretty_print=True,
+            encoding=_encoding, xml_declaration=True))
 
-    summary = {
-        'dtd_errors': [_make_err_message(err) for err in dtd_errors],
-        'sps_errors': [_make_err_message(err) for err in sps_errors],
-        'is_valid': bool(dtd_is_valid and sps_is_valid),
-    }
+    def summarize(self):
+        """Produce a summarized result of the validation.
+        """
+        validator = self.validator
 
-    if assets_basedir:
-        LOGGER.info('looking for assets in %s' % (assets_basedir,))
-        summary['assets'] = validator.lookup_assets(assets_basedir)
-        LOGGER.info('total assets referenced: %s' % (len(summary['assets']),))
+        def _make_err_message(err):
+            """ An error message is comprised of the message itself and the
+            element sourceline.
+            """
+            err_msg = {'message': err.message}
 
-    return summary
+            try:
+                err_element = err.get_apparent_element(validator.lxml)
+            except ValueError:
+                LOGGER.info('Could not locate the element name in: %s', err.message)
+                err_element = None
+
+            if err_element is not None:
+                err_msg['apparent_line'] = err_element.sourceline
+            else:
+                err_msg['apparent_line'] = None
+
+            return err_msg
+
+        dtd_is_valid, dtd_errors = validator.validate()
+        sps_is_valid, sps_errors = validator.validate_style()
+
+        summary = {
+            'dtd_errors': [_make_err_message(err) for err in dtd_errors],
+            'sps_errors': [_make_err_message(err) for err in sps_errors],
+            'is_valid': bool(dtd_is_valid and sps_is_valid),
+        }
+
+        if self.assets_basedir:
+            LOGGER.info('looking for assets in %s', self.assets_basedir)
+            summary['assets'] = validator.lookup_assets(self.assets_basedir)
+            LOGGER.info('total assets referenced: %s', len(summary['assets']))
+
+        return summary
 
 
 @packtools.utils.config_xml_catalog
@@ -113,42 +138,38 @@ def _main():
     input_args = args.XML or sys.stdin
     summary_list = []
 
+
     for xml in packtools.utils.flatten(input_args):
-        LOGGER.info('starting validation of %s' % (xml,))
+        LOGGER.info('starting validation of %s', xml)
 
         try:
-            xml_validator = get_xmlvalidator(xml, args.nonetwork, args.extrasch)
+            stylechecker = StyleChecker.fromfile(xml,
+                    assets_basedir=args.assetsdir, nonetwork=args.nonetwork,
+                    extrasch=args.extrasch)
 
         except (etree.XMLSyntaxError, exceptions.XMLDoctypeError,
                 exceptions.XMLSPSVersionError) as exc:
-            LOGGER.debug(exc)
+            LOGGER.exception(exc)
             print(ERR_MESSAGE.format(filename=xml, details=exc),
                     file=sys.stderr)
             continue
 
+
         if args.annotated:
-            err_xml = xml_validator.annotate_errors()
 
             fname, fext = xml.rsplit('.', 1)
             out_fname = '.'.join([fname, 'annotated', fext])
 
             with open(out_fname, 'wb') as fp:
-                fp.write(etree.tostring(err_xml, pretty_print=True,
-                            encoding='utf-8', xml_declaration=True))
+                stylechecker.annotate(fp)
 
             print('Annotated XML file:', out_fname)
 
         else:
             try:
-                # remote XML will not lookup for assets
-                if xml.startswith(('http:', 'https:')):
-                    assets_basedir = None
-                else:
-                    assets_basedir = args.assetsdir or os.path.dirname(xml)
-
-                summary = summarize(xml_validator, assets_basedir=assets_basedir)
+                summary = stylechecker.summarize()
             except TypeError as exc:
-                LOGGER.debug(exc)
+                LOGGER.exception(exc)
                 LOGGER.warning(
                         'Error validating %s. Skipping. Run with DEBUG for more info.',
                         xml)
@@ -161,7 +182,7 @@ def _main():
             else:
                 summary_list.append(summary)
 
-        LOGGER.info('finished validating %s' % (xml,))
+        LOGGER.info('finished validating %s', xml)
 
     if summary_list:
         print(packtools.utils.prettify(summary_list, colorize=args.nocolors))
@@ -172,7 +193,9 @@ def main():
         _main()
     except KeyboardInterrupt:
         LOGGER.debug('The program is terminating due to SIGTERM.')
-        pass
+    except Exception as exc:
+        LOGGER.exception(exc)
+        sys.exit('An unexpected error has occurred.')
 
 
 if __name__ == '__main__':
